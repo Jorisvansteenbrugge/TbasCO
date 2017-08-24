d2322d3554f08b477540d777106b8cd9704ee18b
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2017-08-24 21:19:56 +0200

    wip

--- R/preprocess.R
+++ R/preprocess.R
@@ -1,33 +1,71 @@
 
 #' Pre-processing of RNAseq data
 #' @export
-Pre_Process <- function(RNAseq.table){
-  RNAseq.table <- Normalization(RNAseq.table)
+Pre_process_input <- function(filepath, normalize = TRUE){
+  RNAseq.table <- read.csv2(filepath)
+
+  # To be sure the corresponding collumns are converted to their correct type
+  # TODO: also for sample columns
+  RNAseq.table$Bin <- as.character(RNAseq.table$Bin)
+
+
+  # Identify some basic features of the table
+  RNAseq.features <- Get_matrix_features(RNAseq.table)
+
+  # Combine the table and the features in one object
+  RNAseq.data <- list("table"    = RNAseq.table,
+                      "features" = RNAseq.features)
+
+
+  return(RNAseq.data)
+
+
+  #RNAseq.table <- Normalization(RNAseq.table)
+}
+
+
+
+Get_matrix_features <- function(RNAseq.table){
+  .Get_sample_columns <- function(column.names){
+    column_contains_Sample <- sapply(column.names,
+                                     function(x) grepl("Sample", x, fixed = T))
+
+    return( as.numeric(which(column_contains_Sample == TRUE)) )
+  }
+
+  sample_columns <- .Get_sample_columns(colnames(RNAseq.table))
+
+  return(list(
+    "bins"                = sort(unique(RNAseq.table$Bin)),
+    "sample_columns"      = sample_columns,
+    "rank_columns"        = ncol(RNAseq.table)+1:length(sample_columns),
+    "KO_presense_absense" = NA
+  ))
 }
 
 
 
 Normalization <- function(RNAseq.table, normalize_function){
 
-  SS <- matrix_features@SS
-  SE <- matrix_features@SE
-  no_feature <- matrix_features@no_feature
-  ambiguous <- matrix_features@ambiguous
-  not_aligned <- matrix_features@not_aligned
-
-  if(method == "default"){
-    return(defaultRNA_Normalize(SS, SE, RNAseq_Annotated_Matrix, no_feature, ambiguous,not_aligned))
-  }
-  else if(method == "TMM" || method == "RLE"){
-    return(edgeRmethods(SS, SE, method, RNAseq_Annotated_Matrix))
+  if(missing(normalize_function)){
+    # Perform the default normalization
   }
 }
 
+#
+# edgeRmethods <- function(SS, SE, method_name, RNAseq_Annotated_Matrix){
+#   library(edgeR)
+#   RNAseq_Annotated_Matrix1<-DGEList(counts=RNAseq_Annotated_Matrix[, SS:SE],lib.size=library_size)
+#   norm_factors <- calcNormFactors(RNAseq_Annotated_Matrix1, method=method_name)
+#   RNAseq_Annotated_Matrix[, SS:SE] <- t(t(as.matrix(norm_factors)) / norm_factors$samples[,3])
+#   return(RNAseq_Annotated_Matrix)
+# }
 
-edgeRmethods <- function(SS, SE, method_name, RNAseq_Annotated_Matrix){
-  library(edgeR)
-  RNAseq_Annotated_Matrix1<-DGEList(counts=RNAseq_Annotated_Matrix[, SS:SE],lib.size=library_size)
-  norm_factors <- calcNormFactors(RNAseq_Annotated_Matrix1, method=method_name)
-  RNAseq_Annotated_Matrix[, SS:SE] <- t(t(as.matrix(norm_factors)) / norm_factors$samples[,3])
-  return(RNAseq_Annotated_Matrix)
-}
+
+
+# setClass('General_features', representation(name                = 'character',
+#                                             bins                = 'vector',
+#                                             sample_columns      = 'vector',
+#                                             rank_columns        = 'vector',
+#                                             KO_presence_absence = 'matrix'),
+#          prototype(name = 'matrix_features'))
