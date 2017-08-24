b757ca7892e51ef7c2af851c88ca966a1ba7c4ca
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2017-08-24 21:19:51 +0200

    no longer needed

--- R/Definitions.R
+++
@@ -1,8 +0,0 @@
-#' Defines the S3 class object
-#'
-setClass('General_features', representation(name                = 'character',
-                                            bins                = 'vector',
-                                            sample_columns      = 'vector',
-                                            rank_columns        = 'vector',
-                                            KO_presence_absence = 'matrix'),
-         prototype(name = 'matrix_features'))
