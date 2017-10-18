231032d5dcdb469068519b8697781eb021615c9b
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2017-10-18 21:05:16 +0200

    initial full version

--- R/Individual_Annotation_Background.R
+++ R/Individual_Annotation_Background.R
@@ -9,65 +9,168 @@ Individual_Annotation_Background <- function(RNAseq.data,
   registerDoSNOW(cl)
 
 
-  result <- foreach::foreach(i = 1: 3, .export = c('bg1',
-                                                   'bg2',
-                                                   'bg3',
+  result <- foreach::foreach(i = 1: 3, .export = c('Random.Genes.bkgd',
+                                                   'Random.Annotated.Genes.bkgd',
+                                                   'Random.Identical.Annotated.Genes.bkgd',
                                                    'N',
                                                    'RNAseq.data',
                                                    'metrics')) %dopar% {
     if(i == 1){
-      bg1(RNAseq.data, metrics)
+      Random.Genes.bkgd(RNAseq.data, metrics, N)
     }else if(i == 2){
-      bg2()
-    }else{
-      bg3()
+      Random.Annotated.Genes.bkgd(RNAseq.data, metrics, N)
+    }else if (i == 3){
+      Random.Identical.Annotated.Genes.bkgd(RNAseq.data, metrics, N)
     }
-                             }
+  }
 
   stopCluster(cl)
-
+  names(result) <- c("Random Genes", "Random Annotated Genes", "Genes with the same annotation")
   return(result)
 }
 
 
 
-bg1 <- function(RNAseq.data, metrics, N){
+Random.Genes.bkgd <- function(RNAseq.data, metrics, N){
+
+  # Creating the output format
+  output <- list()
+  for(metric in names(metrics)){
+    output[[metric]] <- rep(NA, N)
+  }
+
 
   for(i in 1:N){
 
-    random.genomes <- sample(RNAseq.data$features$bins, 2)
+    random.genomes     <- sample(RNAseq.data$features$bins, 2)
 
     positions.genome.A <- which(RNAseq.data$table$Bin == random.genomes[1])
     positions.genome.B <- which(RNAseq.data$table$Bin == random.genomes[2])
 
-    #sample one gene from both
-    position.A <- sample(positions.genome.A, 1)
-    position.B <- sample(positions.genome.B, 1)
+    position.A         <- sample(positions.genome.A, 1)
+    position.B         <- sample(positions.genome.B, 1)
 
+    for(metric.current in names(metrics)){
+      print(metric.current)
+      row.A <- RNAseq.data$table[position.A, ]
+      row.B <- RNAseq.data$table[position.B, ]
+
+      # Call the distance metric function
+      distance <- metrics[[metric.current]](row.A, row.B, RNAseq.data$features)
+      print(distance)
+      output[[metric.current]][i] <- distance
+    }
+  }
+
+  return(output)
+}
+
+Random.Annotated.Genes.bkgd <- function(RNAseq.data, metrics, N){
+
+  # Creating the output format
+  output <- list()
+  for(metric in names(metrics)){
+    output[[metric]] <- rep(NA, N)
+  }
+
+
+    for(i in 1:N){
+
+    random.genomes     <- sample(RNAseq.data$features$bins, 2)
+
+    positions.genome.A <- which(RNAseq.data$table$Bin == random.genomes[1] &
+                                RNAseq.data$table$Annotation != "")
+
+    positions.genome.B <- which(RNAseq.data$table$Bin == random.genomes[2] &
+                                RNAseq.data$table$Annotation != "")
+
+    position.A         <- sample(positions.genome.A, 1)
+    position.B         <- sample(positions.genome.B, 1)
+
+
+    for(metric.current in names(metrics)){
+      print(metric.current)
+      row.A <- RNAseq.data$table[position.A, ]
+      row.B <- RNAseq.data$table[position.B, ]
+
+      # Call the distance metric function
+      distance <- metrics[[metric.current]](row.A, row.B, RNAseq.data$features)
+      print(distance)
+      output[[metric.current]][i] <- distance
+    }
 
   }
 
+  return(output)
 }
 
-bg2 <- function(){
-  return(rep("2", 10))
-}
+Random.Identical.Annotated.Genes.bkgd <- function(RNAseq.data, metrics, N){
 
-bg3 <- function(){
-  return(rep("3", 10))
+  # Creating the output format
+  output <- list()
+  for(metric in names(metrics)){
+    output[[metric]] <- rep(NA, N)
+  }
+
+
+  for(i in 1:N){
+
+    random.genomes     <- sample(RNAseq.data$features$bins, 2)
+
+    positions.genome.A <- RNAseq.data$table[which(RNAseq.data$table$Bin == random.genomes[1] &
+                                  RNAseq.data$table$Annotation != ""), ]
+
+    positions.genome.B <- RNAseq.data$table[which(RNAseq.data$table$Bin == random.genomes[2] &
+                                  RNAseq.data$table$Annotation != ""), ]
+
+    pool.A <- as.character(positions.genome.A$Annotation)
+    pool.B <- as.character(positions.genome.B$Annotation)
+
+    annotations.overlap <- pool.A[pool.A %in% pool.B]
+    random.annotation   <- sample(annotations.overlap, 1)
+
+    # Take the first occurence
+    position.genome.A <-
+      positions.genome.A[which(positions.genome.A$Annotation == random.annotation), ] [1,]
+
+    position.genome.B <-
+      positions.genome.B[which(positions.genome.B$Annotation == random.annotation), ] [ 1, ]
+
+    # Tmp fix
+    position.A         <- position.genome.A
+    position.B         <- position.genome.B
+
+
+    for(metric.current in names(metrics)){
+      print(metric.current)
+      row.A <- position.A
+      row.B <- position.B
+
+      # Call the distance metric function
+      distance <- metrics[[metric.current]](row.A, row.B, RNAseq.data$features)
+      print(distance)
+      output[[metric.current]][i] <- distance
+    }
+
+  }
+
+  return(output)
 }
 
 
 
 # Example metrics
 PC <- function(rowA, rowB, RNAseq.features){
-  return(cor(rowA[RNAseq.features$sample.columns],
-             rowB[RNAseq.features$sample.columns]
+  return(cor(as.numeric(rowA[RNAseq.features$sample.columns]),
+             as.numeric(rowB[RNAseq.features$sample.columns])
              )
          )
 }
 
 NRED <- function(rowA, rowB, RNAseq.features) {
-  sum((rowA[RNAseq.features$rank.columns] - rowB[RNAseq.features$rank.columns])
-      * (rowA[RNAseq.features$rank.columns] - rowB[RNAseq.features$rank.columns]))
+  r.A <- as.numeric(rowA[RNAseq.features$rank.columns])
+  r.B <- as.numeric(rowB[RNAseq.features$rank.columns])
+  return(
+    sum((r.A - r.B) * (r.A - r.B))
+  )
 }
