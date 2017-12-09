8b28ad084d6524aedacafe2460972c99fb01afb9
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2017-12-09 10:57:47 +0100

    Working Z_scores"

--- R/Calc_Z_scores.R
+++ R/Calc_Z_scores.R
@@ -7,12 +7,13 @@ Calc_Z_scores <- function(bkgd.individual, metrics){
 
   # This can probably be a lot shorter
   for(distribution in names(bkgd.individual)){
+
     mu.values[[distribution]] <- list()
     sd.values[[distribution]] <- list()
 
     current.bkgd <- bkgd.individual[[distribution]]
 
-    for(metric in metrics){
+    for(metric in names(metrics)){
       mu    <- mean(current.bkgd[[metric]])
       stdev <- sd(current.bkgd[[metric]])
 
@@ -22,26 +23,25 @@ Calc_Z_scores <- function(bkgd.individual, metrics){
 
   }
 
+
   #applying them
   distributions <- c("Random Annotated Genes",
                      "Genes with the same annotation")
-  Z_scores.output <- list("Z_Random Annotated Genes" = list(),
-                 "Z_Genes with the same annotation" = list())
-  for(distribution in distributions){
-    current.bkgd <- bkgd.individual[[distribution]]
+  Z_scores.output <- list()
 
-    for(metric in metrics){
+  for(distribution in distributions){
+    Z_scores.output[[distribution]] <- list()
+
+    current.bkgd <- bkgd.individual[[distribution]]
+    for(metric in names(metrics)){
+
       current.idv.bkgd <- bkgd.individual[[distribution]][[metric]]
       current.mu <- mu.values[[distribution]][[metric]]
       current.sd <- sd.values[[distribution]][[metric]]
 
       Z_scores.output[[distribution]][[metric]] <- (current.idv.bkgd - current.mu) /
         current.sd
-
-
     }
-
   }
-
   return(Z_scores.output)
 }
