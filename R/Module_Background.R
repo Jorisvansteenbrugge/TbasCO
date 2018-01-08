125048ecf63fba3ed24e136d288d32b1e249dee2
Joris <joris.vansteenbrugge@wur.nl>
2018-01-08 11:39:23 +0100

    Add files via upload

---
+++ R/Module_Background.R
@@ -0,0 +1,16 @@
+#' @author JJM van Steenbrugge
+#' @param RNAseq.data
+#' @param N Number of genes to include in the random module
+#' @param Z Number of iterations
+#' @param range NOT SUPPORTED YET
+#' @param metrics Named list containing possibly multiple functions for distance
+#' @param threads Number of cpu cores to be used
+#' @export
+Module_Background <- function(RNAseq.data,
+                              Z_scores,
+                              N,
+                              Z,
+                              metrics,
+                              threads = 2){
+
+}
