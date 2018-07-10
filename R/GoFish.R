8bee2fa2c790666182e2f1b8590d2e4b2da3d43c
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2018-07-10 14:04:33 +0200

    wip

--- R/GoFish.R
+++ R/GoFish.R
@@ -2,42 +2,54 @@
 #' or more genomes
 #' @name Go Fish
 #' @description Identify whether a gene has a certain expression pattern in one or more genomes.
-#' @param trait character string of the Annotation term
 #' @param RNAseq.data Collection of multple components, include RNA seq data, annotations, etc. See \code{\link{Pre_process_input}} for the full list.
-#' @param pattern character string of the kind of pattern to look for. The string
-#' should be one of the following: {'increase', 'decrease', 'peak', 'vale}
 #' @export
 #' @example Go_Fish('K00927', RNAseq.data, type = 'peak')
 #' @author JJM van Steenbrugge
-Go_Fish <- function(trait, RNAseq.data, pattern){
+Go_Fish <- function(RNAseq.data){
 
-  if(missing(pattern)){
-    points <- .Draw_pattern()
-    lines(points$x, points$y)
-    text(3,0.5,'Using this pattern to match')
+  points <- .Draw_pattern()
+  lines(points$x, points$y)
+  text(3,0.5,'Using this pattern to match')
+
+  for (i in 1:length(names(RNAseq.data$features$annotation.db$module.dict))) {
+    trait <- RNAseq.data$features$annotation.db$module.dict[[i]]
+    genes <- RNAseq.data$table[which(RNAseq.data$table$Annotation %in% trait),]
+
+
+    verdicts <- apply(genes, 1, function(gene){
+      gene <- gene[RNAseq.data$features$rank.columns]
+      return(.Test_pattern(points$y, gene))
+    })
+
+    if (TRUE %in% verdicts) {
+      print(names(RNAseq.data$features$annotation.db$module.dict)[i])
+    }
   }
-
-
-
-  annotations <- RNAseq.data$features$annotation.db$module.dict[[trait]]
-
-  annotation.rna <- RNAseq.data$table[which(
-  RNAseq.data$table$Annotation %in% annotations),]
-
-  tfs <- apply(annotation.rna, 1, .process_row)
-  return(tfs)
 
 }
 
+.Test_pattern <- function(pattern, gene_pattern, margin = 0.15){
+  verdict <- T
+  for( i in 1: length(pattern) ) {
+    lower <- pattern[i] - margin
+    upper <- pattern[i] + margin
+
+    if(gene_pattern[i] < lower || gene_pattern[i] > upper) {
+      verdict <- F
+    }
+
+  }
+  return(verdict)
+}
 
 .Draw_pattern <- function(nsamples = 6){
   plot(c(1,nsamples), c(0,1), type='n',
        xlab = 'Time points',
        ylab = 'Normalized Rank')
-  cat("Click on six points in the graph, then press finish in the plot window")
+  cat("Pick a rank value (Y axis) for each timme point by clicking in the graph")
 
   return(locator(nsamples))
 
 }
 
-.Draw_pattern()
