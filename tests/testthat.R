5dc8ff2e1014efe73ac1f2d55065cd2055588fdd
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2018-08-03 17:00:19 +0200

    updates

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,12 @@
+library(magrittr)
 library(testthat)
 library(TcT)
 
+RNAseq.data <- Pre_process_input(url("http://jorisvansteenbrugge.com/sample_data.csv"),
+                                 url("http://jorisvansteenbrugge.com/KEGG_modules.tsv"),
+                                 normalize.method    = T,
+                                 filter.method       = 'MAD',
+                                 filter.low.coverage = T,
+                                 normalization.features)
 
 test_check("TcT")
