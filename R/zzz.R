4ee39e1d35cdb869f1ef5beb16859f369d7fc72c
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2018-08-16 10:42:23 +0200

    windows support

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,17 @@
 .onLoad <- function(libname, pkgname){
-  assign('file.path', paste0(libname,"/TcT/data/sample_data.csv"), envir = .GlobalEnv)
-  assign('annotation.db.path', paste0(libname,'/TcT/data/KEGG_modules.tsv'), envir = .GlobalEnv)
-  assign('ko.db.path', paste0(libname,'/TcT/data/KO_identifiers.keg'), envir = .GlobalEnv)
-  load(paste0(libname,'/TcT/data/sub_modules.RData'), envir = .GlobalEnv)
+  os <- .Platform$OS.type
+  if(os == "unix"){
+    assign('file.path', paste0(libname,"/TcT/data/sample_data.csv"), envir = .GlobalEnv)
+    assign('annotation.db.path', paste0(libname,'/TcT/data/KEGG_modules.tsv'), envir = .GlobalEnv)
+    assign('ko.db.path', paste0(libname,'/TcT/data/KO_identifiers.keg'), envir = .GlobalEnv)
+    load(paste0(libname,'/TcT/data/sub_modules.RData'), envir = .GlobalEnv)
+  }else if(os == 'windows'){
+    assign('file.path', paste0(libname,"\\TcT\\data\\sample_data.csv"), envir = .GlobalEnv)
+    assign('annotation.db.path', paste0(libname,'\\TcT\\data\\KEGG_modules.tsv'), envir = .GlobalEnv)
+    assign('ko.db.path', paste0(libname,'\\TcT\\data\\KO_identifiers.keg'), envir = .GlobalEnv)
+    load(paste0(libname,'\\TcT\\data\\sub_modules.RData'), envir = .GlobalEnv)
+  }
+
 }
 
 .onDetach <- function(libname, pkgname){
