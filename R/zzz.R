8d7a8cc1c7219f0e07cf0ff9adc75c6532315655
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2019-01-29 16:27:14 +0100

    fixed issue caused by package rename

--- R/zzz.R
+++ R/zzz.R
@@ -2,15 +2,16 @@
   library(magrittr)
   os <- .Platform$OS.type
   if(os == "unix"){
-    assign('file.path', paste0(libname,"/TcT/data/sample_data.csv"), envir = .GlobalEnv)
-    assign('annotation.db.path', paste0(libname,'/TcT/data/KEGG_modules.tsv'), envir = .GlobalEnv)
-    assign('ko.db.path', paste0(libname,'/TcT/data/KO_identifiers.keg'), envir = .GlobalEnv)
-    load(paste0(libname,'/TcT/data/sub_modules.RData'), envir = .GlobalEnv)
+    assign('file.path', paste0(libname,"/TbasCO/data/sample_data.csv"), envir = .GlobalEnv)
+    assign('annotation.db.path', paste0(libname,'/TbasCO/data/KEGG_modules.tsv'), envir = .GlobalEnv)
+    assign('ko.db.path', paste0(libname,'/TbasCO/data/KO_identifiers.keg'), envir = .GlobalEnv)
+
+    load(paste0(libname,'/TbasCO/data/sub_modules.RData'), envir = .GlobalEnv)
   }else if(os == 'windows'){
-    assign('file.path', paste0(libname,"\\TcT\\data\\sample_data.csv"), envir = .GlobalEnv)
-    assign('annotation.db.path', paste0(libname,'\\TcT\\data\\KEGG_modules.tsv'), envir = .GlobalEnv)
-    assign('ko.db.path', paste0(libname,'\\TcT\\data\\KO_identifiers.keg'), envir = .GlobalEnv)
-    load(paste0(libname,'\\TcT\\data\\sub_modules.RData'), envir = .GlobalEnv)
+    assign('file.path', paste0(libname,"\\TbasCO\\data\\sample_data.csv"), envir = .GlobalEnv)
+    assign('annotation.db.path', paste0(libname,'\\TbasCO\\data\\KEGG_modules.tsv'), envir = .GlobalEnv)
+    assign('ko.db.path', paste0(libname,'\\TbasCO\\data\\KO_identifiers.keg'), envir = .GlobalEnv)
+    load(paste0(libname,'\\TbasCO\\data\\sub_modules.RData'), envir = .GlobalEnv)
   }
 
 }
