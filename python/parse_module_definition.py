66c0f9281ad562c613818384f7f3586082ce8eb6
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2019-07-24 10:50:55 +0200

    wip

--- python/parse_module_definition.py
+++ python/parse_module_definition.py
@@ -15,9 +15,11 @@ from sympy.logic.boolalg import to_dnf
 from sympy import symbols
 from bs4 import BeautifulSoup
 
+
 def pprint(l):
     for val in l:
         print(val)
+
 
 def retrieve_definition(module_name):
     """Retrieves the html page based on the database entry. The module definition
@@ -52,6 +54,7 @@ def retrieve_definition(module_name):
             break
     return definition
 
+
 def clean_definition(definition):
     """Give the definition without any optional terms.
        Optional terms are indicated using '-' (e.g. (A-B) C <- A optional B AND C)
@@ -61,12 +64,11 @@ def clean_definition(definition):
                            annotations (e.g. (A,B) C -> (A OR B) AND C).
 
     """
-    if '-' not in definition: #then we don't need cleaning
+    if '-' not in definition:  # then we don't need cleaning
         return definition
 
     # double -- quick and dirty fix
-    definition = definition.replace(" -- --","").replace(" -- ","-").replace("-- ",'-').replace(" --","")
-
+    definition = definition.replace(" -- --", "").replace(" -- ", "-").replace("-- ", '-').replace(" --", "")
 
     # Keep doing this until all optional terms are gone
     while '-' in definition:
@@ -74,18 +76,18 @@ def clean_definition(definition):
 
         # If there is only one KO term afterwards we skip ahead the length of the
         # KO term to remove itself.
-        if definition[min_pos+1] == 'K':
-            definition = definition.replace(definition[min_pos:min_pos+7], "") #remove the -K
+        if definition[min_pos + 1] == 'K':
+            definition = definition.replace(definition[min_pos:min_pos + 7], "")  # remove the -K
             continue
 
         # In this case, there are multiple KO terms afterwards, so we want to
         # remove them all.
-        elif definition[min_pos+1] == '(':
+        elif definition[min_pos + 1] == '(':
 
             opens = ['(']
             end_pos = None
             # We go find the corresponding closing parenthesis
-            for idx in range(min_pos+2, len(definition)):
+            for idx in range(min_pos + 2, len(definition)):
                 char = definition[idx]
                 if char == ')':
                     opens.pop()
@@ -99,17 +101,16 @@ def clean_definition(definition):
 
             if end_pos:
                 # Remove all characters between the optional parentheses
-                definition = definition.replace(definition[min_pos:end_pos+1], "")
+                definition = definition.replace(definition[min_pos:end_pos + 1], "")
         else:
-            raise ValueError("Unsupported optional term found", definition[min_pos+1])
-
+            raise ValueError("Unsupported optional term found", definition[min_pos + 1])
 
     definition = definition.replace("  ", " ")
     if definition.startswith(' '):
         definition = definition[1:]
 
     while definition.endswith(" "):
-        definition = definition[0:len(definition)-1]
+        definition = definition[0:len(definition) - 1]
 
     return definition.replace("  ", " ")
 
@@ -123,9 +124,10 @@ def get_symbol_dict(definitions):
 
     """
     ko_symbols = re.findall(r'K[0-9]{5}', definitions)
-    symbol_dict = {ko:symbols(ko) for ko in ko_symbols}
+    symbol_dict = {ko: symbols(ko) for ko in ko_symbols}
 
     return symbol_dict
+
 
 def parse_definition(definitions):
     """Convert a definitions set to its disjunctive normal form
@@ -137,7 +139,15 @@ def parse_definition(definitions):
     symbol_dict = get_symbol_dict(definitions)
 
     expr_text = definitions.replace(" ", " & ").replace(",", " | ").replace('+', ' & ')
-    expr = parse_expr(expr_text, local_dict = symbol_dict)
+    if expr_text.endswith('& ') or expr_text.endswith('| '):
+        expr_text = expr_text[:-3]
+        print(expr_text)
+    try:
+        expr = parse_expr(expr_text, local_dict=symbol_dict)
+    except SyntaxError:
+        print(definitions)
+        print(expr_text)
+        return
 
     dnf = to_dnf(expr)
     combinations = [d.strip("() ").replace(" ", "") for d in str(dnf).split("|")]
@@ -145,6 +155,7 @@ def parse_definition(definitions):
     combinations = [x.split("&") for x in combinations]
 
     return combinations
+
 
 def parse_module(module_name):
     definition = retrieve_definition(module_name)
@@ -154,6 +165,26 @@ def parse_module(module_name):
     try:
         definition_parsed = parse_definition(definition)
     except ValueError as err:
-            print(err.args)
+        print(err.args)
+        definition_parsed = []
 
     return definition_parsed
+
+
+if __name__ == '__main__':
+    mod_list = []
+    with open('../data/kegg_modules_2019_07_23.tsv') as in_file:
+        in_file.readline()
+        for line in in_file:
+            line = line.rstrip().split()
+            mod_list.append(line[0])
+
+    mod_list = list(set(mod_list))
+
+    with open("../data/kegg_modules_2019_07_23_DNF_flast.tsv", 'w') as dnf_file:
+        for module in mod_list:
+            print(module)
+            options = parse_module(module)
+
+            opt_line = ";".join([','.join(option) for option in options])
+            dnf_file.write(f"{module};{opt_line}\n")
